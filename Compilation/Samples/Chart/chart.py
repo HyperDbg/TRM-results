6a17760ee2826269b830180b05cbc4c3a641f108
Sinaei <sina@hyperdbg.org>
2024-01-10 12:53:31 +0900

    update

--- Compilation/Samples/Chart/chart.py
+++ Compilation/Samples/Chart/chart.py
@@ -33,24 +33,30 @@ for file_path in file_paths:
 # Use a colormap for the lines
 cmap = get_cmap('tab10')
 
-markers=['v','^','o', '+']
-lines=['dashed','dashed','dashed', 'dashed']
+markers=['o', 'o', 'o', 'o']
+lines=['solid','solid','solid', 'solid']
 
-lw = 10
+lw = 5
 overlapping = 1
 
 # Create a separate plot for each file with unique colors
 for i in range(len(file_paths)):
     color = cmap(i % 10)  # Use modulo to ensure colors repeat if more than 10 files
     overlapping = overlapping - 0.1
-    lw = lw - 2
-    plt.plot(all_hex_numbers[i], color=color, marker=markers[i], label=file_names[i], alpha=overlapping, linestyle=lines[i], linewidth=lw)
-
+    lw = lw - 1
+    plt.plot(all_hex_numbers[i], color=color, label=file_names[i], alpha=overlapping, linestyle=lines[i], linewidth=lw)
+    
+    # Fill the area under the line
+    plt.fill_between(range(len(all_hex_numbers[i])), all_hex_numbers[i], color=color, alpha=0.2)
 
 # Add labels and legend
-plt.title('Linear chart based on relative memory offset')
-plt.xlabel('Time')
-plt.ylabel('Relative memory offset')
-plt.grid(True)
-plt.legend()
+plt.xlabel('Time', fontsize=20)
+plt.ylabel('Relative memory offset', fontsize=20)
+
+# Add grid with borders
+plt.grid(True, which='both', linestyle='-', linewidth=0, color='black')
+
+# Set legend font size and position
+plt.legend(loc='upper right', bbox_to_anchor=(0.95, 0.93), prop={'size': 20})  # Adjust the font size here
+
 plt.show()
