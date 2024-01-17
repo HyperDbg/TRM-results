387857732c62704255e6ea4ce1370b9cef0c76ed
Sinaei <sina@hyperdbg.org>
2024-01-17 15:18:00 +0900

    add source link

--- In-memory/MiscTechniques/MiscTechniques/MiscTechniques.c
+++ In-memory/MiscTechniques/MiscTechniques/MiscTechniques.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include "Techniques.h"
 
+//
+// The techniques mentioned in this project are all derived from: https://www.ired.team
+//
+
 int main()
 {
 	UINT32 TargetProcessId = 0x1c0;
